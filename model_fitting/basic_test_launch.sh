079a00748090fe084990d7f40e48d7b15839301e
erin-n-gilbertson <erin.n.gilbertson>
2024-10-04 09:16:51 -0400

    new args order

--- model_fitting/basic_test_launch.sh
+++ model_fitting/basic_test_launch.sh
@@ -17,9 +17,9 @@ conda activate scmpra
 tempdir=/home/eng26/palmer_scratch/scmpra_temp
 counts=/home/eng26/project/scmpra/data/shendure_mpra_counts_GSE217686.parq
 id=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $1}' test_params.txt)
-formula=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $2}' test_params.txt)
-maxiter=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $3}' test_params.txt)
-regfit=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $4}' test_params.txt)
-model=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $5}' test_params.txt)
+formula=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $4}' test_params.txt)
+maxiter=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $5}' test_params.txt)
+regfit=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $2}' test_params.txt)
+model=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $3}' test_params.txt)
 
 python basic_test.py --scmpra_counts_file $counts --model_choice $model --formula $formula --maxiter $maxiter --regularized_fit $regfit --temp_dir $tempdir --out_file $id_$model$maxiter_$regfit_$formula
