52cfdd39b833776305e1c0cd6a38da2544f3457b
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-02-24 12:56:57 -0500

    more skeleton

--- scMPRAforge/core.py
+++ scMPRAforge/core.py
@@ -5,13 +5,15 @@
 
 #external imports
 import seaborn as sns
+import pandas as pd
+import numpy as np
 
 #internal imports
 from .utils import unimplemented
 
 @unimplemented
 def always_unfinished():
-    """tests unimplemented decorator"""
+    """tests unimplemented decorator."""
     pass
 
 
@@ -19,45 +21,106 @@ def helloworld():
     print("hello world!")
     pass
 
+
 @unimplemented
-def load_hypothesis_set():
+def table_type(column_names):
+    """
+    Arguments:
+        column_names <pd.Index>
+    Returns:
+        <str>
+
+    Returns the putative table type, one of:
+    - mpra_umiwise
+    - mpra_readwise
+    - hypotheses
+    - results
+    - malformed
+
+    Is kind with respect to extra columns & optional columns. 
+    
+    (We could extend to type-checking as well, but that seems a tad draconian.)
+    """
     pass
 
 @unimplemented
-def load_scMPRA_data(filepath,wise,collapsed):
+def load_hypothesis_set(filepath):
     """
-        (for development, assume read-wise, uncollapsed)
-        Takes 
-        
+    Arguments
+        filepath <str>
+    Returns
+        <pd.DataFrame>
+
+    Loads a hypothesis or hypothesis+results set from disc.
+    """
+    #load table...
+    #assert table_type(table.columns)==
+    pass
+
+
+
+@unimplemented
+def load_scMPRA_data(filepath):
+    """
+    Arguments
+        filepath <str>
+    Returns
+        <pd.DataFrame>
+
+    Loads scMPRA data from `filepath`. Determines whether it is umi or read-wise.
+    
     """
     pass
+
+#        1         2         3         4         5         6         7         8
+#2345678901234567890123456789012345678901234567890123456789012345678901234567890
 
 @unimplemented
 def graph_qc_metrics(scmpra_data, *args, **kwargs):
     """
-        Takes `scmpra_data`, a pandas dataframe of read-wise MPRA data 
-        (see docs) and plots a histogram of frequency of reads per UMI
-        using seaborn.histplot. All other arguments are passed to
-        the histplot call to allow graph customization.
+    Arguments
+        scmpra_data <pd.DataFrame>
+        *args
+        **kwargs
+    Returns
+        <matplotlib.axes._axes.Axes>
+
+    Takes `scmpra_data`, a pandas dataframe of read-wise MPRA data (see docs) 
+    and returns a histogram of frequency of reads per UMI using seaborn.histplot. 
+    All other arguments are passed to the histplot call to allow graph 
+    customization.
     """
+    assert table_type(scmpra_data.columns) == "mpra_readwise"
     pass
 
 @unimplemented
 def cut_chimeric_reads(scmpra_data, threshold):
     """
-        Takes `scmpra_data`, a pandas dataframe of read-wise scMPRA data 
-        (see docs) and `threshold` <int>.
-        Returns a pandas dataframe of umi-wise MPRA data (see docs)
-        subset to those UMIs which lie above the number-of-reads
-        threshold.
+    Arguments
+        `scmpra_data` <pandas.DataFrame> of read-wise scMPRA data 
+        `threshold` <int>.
+    
+    Returns
+        a pandas dataframe of umi-wise MPRA data
+    
+    subsets to those UMIs which lie above the number-of-reads threshold, 
+    removing chimeric reads. 
     """
+    assert table_type(scmpra_data.columns) == "mpra_readwise"
     pass
 
 @unimplemented
 def hypothesis_tester(scmpra_data, hypotheses, flavor="wald"):
     """
-        Takes scmpra_data, a pandas dataframe of umi-wise scMPRA data
+    Arguments
+        scmpra_data: 
+        hypotheses :
+    `flavor`, a string with values 
+
+    a pandas dataframe of umi-wise scMPRA data
     """
+    assert table_type(scmpra_data.columns) == "mpra_umiwise"
+    assert table_type(hypotheses.columns) == "hypotheses"
     pass
 
 @unimplemented
