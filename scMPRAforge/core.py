6b0fc2406fb169d4eddab532b3f622970cdb0b87
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-02-23 20:42:19 -0500

    added package skeleton

---
+++ scMPRAforge/core.py
@@ -0,0 +1,16 @@
+#all the main functions.
+
+from .utils import unimplemented
+
+@unimplemented
+def analyze_mpra_data():
+    """Placeholder for future function to analyze MPRA data."""
+    pass
+
+
+def helloworld():
+    print("hello world!")
+    pass
+
+
+
---
+++ scMPRAforge/utils.py
@@ -0,0 +1,25 @@
+#utility functions
+#
+
+import functools
+
+unimplemented_functions = []  # List to track unimplemented functions
+
+def unimplemented(func):
+    """
+    Decorator to mark functions as unimplemented.
+    Adds them to a global tracking list and raises a warning when called.
+    """
+    global unimplemented_functions
+    unimplemented_functions.append(func.__name__)
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        raise NotImplementedError
+        return None  # Placeholder return value
+
+    return wrapper
+
+def list_unimplemented():
+    """Returns the list of unimplemented functions."""
+    return unimplemented_functions
