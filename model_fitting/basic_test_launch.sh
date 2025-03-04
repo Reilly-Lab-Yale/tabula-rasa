cf4d3e42c86de5dd831c0cfd5d068483c5c1856f
erin-n-gilbertson <erin.gilbertson@yale.edu>
2025-03-04 11:03:54 -0500

    updated individual model tests

--- model_fitting/basic_test_launch.sh
+++ model_fitting/basic_test_launch.sh
@@ -1,11 +1,11 @@
 #! /bin/bash -l
 
 #SBATCH -J 10_03_2024
-#SBATCH --mem=32G
-#SBATCH --time=20:00:00
+#SBATCH --mem=64G
+#SBATCH --time=48:00:00
 #SBATCH -o /home/eng26/project/scmpra/bin/tabula-rasa/stdout/testing_round4_%A_%a.out
 #SBATCH -e /home/eng26/project/scmpra/bin/tabula-rasa/stdout/testing_round4_%A_%a.err
-#SBATCH --array 1-12
+#SBATCH --array 1-2
 
 
 module load miniconda
@@ -14,16 +14,16 @@ conda activate scmpra
 
 
 tempdir=/home/eng26/palmer_scratch/scmpra_temp
-counts=/home/eng26/project/scmpra/data/shendure_mpra_counts_GSE217686.parq
-#counts=/home/eng26/project/scmpra/data/shendure_counts_grouped_cardio.txt
+counts=/home/eng26/project/scmpra/data/shendure_mpra_counts_grouped_GSE217686.parq
+
+
 id=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $1}' test_params.txt)
-formula=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $4}' test_params.txt)
+model=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $2}' test_params.txt)
+formula=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $3}' test_params.txt)
+zi_param=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $4}' test_params.txt)
 maxiter=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $5}' test_params.txt)
-#regfit=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $2}' test_params.txt)
-model=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $3}' test_params.txt)
+# cre=$(awk -v row=$SLURM_ARRAY_TASK_ID 'NR == row {print $6}' CRE_indi_test_params.txt)
+# counts=/home/eng26/project/scmpra/data/shendure_mpra_counts_grouped_${cre}.parq
 
-#echo 'regfit: ' $regfit
+python basic_test.py --scmpra_counts_file $counts --model_choice $model --formula $formula --maxiter $maxiter --zi_param $zi_param --temp_dir $tempdir --out_file ${id}_GROUPED_${model}${maxiter}_${formula}_${zi_param}
 
-python basic_test.py --scmpra_counts_file $counts --model_choice $model --formula $formula --maxiter $maxiter --regularized_fit $regfit --temp_dir $tempdir --out_file ${id}GROUPED_${model}${maxiter}_${regfit}_${formula}
-
-#python basic_test_bambi.py --scmpra_counts_file $counts --model_choice $model --formula $formula --maxiter $maxiter --temp_dir $tempdir --out_file ${id}_cardio_${model}${maxiter}_${formula}
