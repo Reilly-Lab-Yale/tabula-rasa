fa465906bcfe1f0dbe537136d3939ca08ff066a8
erin-n-gilbertson <erin.gilbertson@yale.edu>
2025-06-30 15:36:10 -0400

    working on hypothesis testing

--- Bootstrapping/compute_empirical_pvalues.py
+++ Bootstrapping/compute_empirical_pvalues.py
@@ -51,7 +51,8 @@ def main():
                    
             # expression values
             exp_cre = df_boot_oi[df_boot_oi['cre_id'] == cre]['max_cluster_mBC_UMI']
-            exp_min_noP = df_boot_oi[df_boot_oi['bootstrap_for_CRE'] == cre]['max_cluster_mBC_UMI']
+            exp_min_noP = df_boot_oi[df_boot_oi['bootstrap_for_CRE'] == cre][df_boot_oi['cre_id'] != cre]['max_cluster_mBC_UMI']
+
 
             if exp_cre.empty or exp_min_noP.empty:
                 continue
@@ -94,59 +95,59 @@ def main():
 
     print(f"Saved activity p-values to {args.output_activity_file}")
 
-    ### === CELL TYPE SPECIFICITY P-VALUES === ###
-    specificity_results = []
+    # ### === CELL TYPE SPECIFICITY P-VALUES === ###
+    # specificity_results = []
 
-    for cre in CRE_list:
-        for rep in biol_reps:
-            df_boot_oi = df_boot[(df_boot['bootstrap_for_CRE'] == cre) & (df_boot['biol_rep'] == rep) & (df_boot['cre_id'] == cre)]
-            df_perm_oi = df_perm[(df_perm['permutation_for_CRE'] == cre) & (df_perm['biol_rep'] == rep) & (df_perm['cre_id'] == cre) & (df_perm['perm_id'] != 'OBS')]
+    # for cre in CRE_list:
+    #     for rep in biol_reps:
+    #         df_boot_oi = df_boot[(df_boot['bootstrap_for_CRE'] == cre) & (df_boot['biol_rep'] == rep) & (df_boot['cre_id'] == cre)]
+    #         df_perm_oi = df_perm[(df_perm['permutation_for_CRE'] == cre) & (df_perm['biol_rep'] == rep) & (df_perm['cre_id'] == cre) & (df_perm['perm_id'] != 'OBS')]
 
-            if df_perm_oi.empty:
-                print(f"Warning: Missing data for cre {cre}, rep {rep}")
-                continue
+    #         if df_perm_oi.empty:
+    #             print(f"Warning: Missing data for cre {cre}, rep {rep}")
+    #             continue
 
-            # FC (fold change) values
-            fc_boot = df_boot_oi['max_cluster_FC_mBC_UMI'].fillna(1)
-            fc_perm = df_perm_oi['max_cluster_FC_mBC_UMI'].fillna(1)
+    #         # FC (fold change) values
+    #         fc_boot = df_boot_oi['max_cluster_FC_mBC_UMI'].fillna(1)
+    #         fc_perm = df_perm_oi['max_cluster_FC_mBC_UMI'].fillna(1)
 
-            # empirical p-value
-            empirical_pvals = calculate_empirical_pvals(fc_boot.values, fc_perm.values)
+    #         # empirical p-value
+    #         empirical_pvals = calculate_empirical_pvals(fc_boot.values, fc_perm.values)
 
-            # Most frequent cluster
-            max_clusters = df_boot_oi['max_expression_cluster_id']
-            if not max_clusters.empty:
-                most_common_cluster = Counter(max_clusters).most_common(1)[0][0]
-                freq_most_common = np.mean(max_clusters == most_common_cluster)
-            else:
-                most_common_cluster = np.nan
-                freq_most_common = np.nan
+    #         # Most frequent cluster
+    #         max_clusters = df_boot_oi['max_expression_cluster_id']
+    #         if not max_clusters.empty:
+    #             most_common_cluster = Counter(max_clusters).most_common(1)[0][0]
+    #             freq_most_common = np.mean(max_clusters == most_common_cluster)
+    #         else:
+    #             most_common_cluster = np.nan
+    #             freq_most_common = np.nan
 
-            specificity_results.append({
-                "cre_id": cre,
-                "biol_rep": rep,
-                "q25_bootstrap_FC_norm": np.quantile(fc_boot, 0.25),
-                "q50_bootstrap_FC_norm": np.quantile(fc_boot, 0.5),
-                "q75_bootstrap_FC_norm": np.quantile(fc_boot, 0.75),
-                "empirical_permute_gex_cluster_p_val": np.mean(empirical_pvals),
-                "most_frequent_max_exp_cluster": most_common_cluster,
-                "frequency_most_frequent_max_exp_cluster": freq_most_common
-            })
+    #         specificity_results.append({
+    #             "cre_id": cre,
+    #             "biol_rep": rep,
+    #             "q25_bootstrap_FC_norm": np.quantile(fc_boot, 0.25),
+    #             "q50_bootstrap_FC_norm": np.quantile(fc_boot, 0.5),
+    #             "q75_bootstrap_FC_norm": np.quantile(fc_boot, 0.75),
+    #             "empirical_permute_gex_cluster_p_val": np.mean(empirical_pvals),
+    #             "most_frequent_max_exp_cluster": most_common_cluster,
+    #             "frequency_most_frequent_max_exp_cluster": freq_most_common
+    #         })
 
-    df_specificity = pd.DataFrame(specificity_results)
+    # df_specificity = pd.DataFrame(specificity_results)
 
-    # FDR correction by biological replicate
-    df_specificity["empirical_permute_gex_cluster_p_val_BH_corr"] = np.nan
-    for rep in biol_reps:
-        mask = df_specificity["biol_rep"] == rep
-        if mask.sum() > 0:
-            corrected = correct_pvals(df_specificity.loc[mask, "empirical_permute_gex_cluster_p_val"])
-            df_specificity.loc[mask, "empirical_permute_gex_cluster_p_val_BH_corr"] = corrected
+    # # FDR correction by biological replicate
+    # df_specificity["empirical_permute_gex_cluster_p_val_BH_corr"] = np.nan
+    # for rep in biol_reps:
+    #     mask = df_specificity["biol_rep"] == rep
+    #     if mask.sum() > 0:
+    #         corrected = correct_pvals(df_specificity.loc[mask, "empirical_permute_gex_cluster_p_val"])
+    #         df_specificity.loc[mask, "empirical_permute_gex_cluster_p_val_BH_corr"] = corrected
 
-    # Save
-    df_specificity.to_csv(args.output_specificity_file, sep="\t", index=False)
+    # # Save
+    # df_specificity.to_csv(args.output_specificity_file, sep="\t", index=False)
 
-    print(f"Saved specificity p-values to {args.output_specificity_file}")
+    # print(f"Saved specificity p-values to {args.output_specificity_file}")
 
 if __name__ == "__main__":
     main()
