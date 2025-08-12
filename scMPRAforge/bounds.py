88d430b36dec4d945d11315e62f3ea517527da71
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-08-12 13:46:07 -0400

    +bounds params

--- scMPRAforge/bounds.py
+++ scMPRAforge/bounds.py
@@ -1,18 +1,29 @@
 from typing import Dict
 from dataclasses import dataclass
 
-@dataclass(frozen=True)
+@dataclass()
 class Bounds:
     """
     Describes the boundaries of an experiment.
+    Specifically the UMI portion.
     """
-    metadata:str
-    min_mpra_umi:float
-    max_mpra_umi:float
-    by_cre_theta:float
-    by_cell_type_theta:float
+
+    metadata:dict=None
+    min_mpra_umi:float=None
+    max_mpra_umi:float=None
+    by_cre_theta:float=None
+    by_cell_type_theta:float=None
+    num_cres:int=None
+    num_cell_types:int=None
+    #mean_mpra_barcodes_per_cre:float
+    #stdev_mpra_barcodes_per_cre:float
+    #barcode_deviation:float
+    #barcode deviation stores how much each barcode affects umi count
+    #ehh... do we really want to model this? probably skip it. 
 
     @classmethod
     def from_ortho(cls):
         pass
 
+#class wet_bounds
+#   read_depth
