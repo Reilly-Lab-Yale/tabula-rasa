f36c32f7204e2d7bebeb11eab04a38481c4093ba
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-08-13 17:10:30 -0400

    +zi & other param

--- scMPRAforge/bounds.py
+++ scMPRAforge/bounds.py
@@ -1,34 +1,43 @@
 from typing import Dict
 from dataclasses import dataclass
 import numpy as np
+import pandas as pd
 
 @dataclass()
 class Bounds:
     """
     Describes the boundaries of an experiment.
     Specifically the UMI portion.
-    """
 
+    by_cell_type_theta and by_cell_type_zi are probably 
+    more accurate for downstream simulation than their 
+    by_cre counterparts, assuming that there are more 
+    cres than cell types, which should be the case for most
+    datasets.
+    """
     metadata:dict=None
+
     min_mpra_umi:float=None
     max_mpra_umi:float=None
     by_cre_theta:float=None
     by_cell_type_theta:float=None
+    by_cre_zi:float=None
+    by_cell_type_zi:float=None
+
     num_cres:int=None
     cells_per_cell_type:dict=None
+
     transfection_nb_mu:float=None
     transfection_nb_alpha:float=None
 
-    #moi will be multiplicity of infection: number of mprabc per cell
+
+    def to_parquet():
+        raise(NotImplementedError)
     
-
-
-    #mean_mpra_barcodes_per_cre:float
-    #stdev_mpra_barcodes_per_cre:float
-    #barcode_deviation:float
-    #barcode deviation stores how much each barcode affects umi count
-    #ehh... do we really want to model this? probably skip it. 
-
+    @classmethod
+    def from_parquet():
+        raise(NotImplementedError)
+    
     @classmethod
     def from_ortho(cls,inp):
         """
@@ -39,6 +48,9 @@ class Bounds:
         This function requires that the ortho still have its training data 
         so we can extract things like "number of cells per cell-type" and 
         "number of MPRA barcodes per cell".
+
+        Note that this function is totally replicate-agnostic. It averages estimated zero 
+        inflation across replicates. 
         """
         
         ret=cls()
@@ -66,6 +78,19 @@ class Bounds:
                 ret.by_cell_type_theta=np.mean(thetas)
             elif var=="by_cre_parameters":
                 ret.by_cre_theta=np.mean(thetas)
+            
+            
+            ## zero inflation ##
+            zis=[]
+            for key in getattr(inp,var).zi:
+                current=getattr(inp,var).zi[key].result()
+                current=current.rename({'zi':key},axis=1)
+                zis.append(current)
+            zis=pd.concat(zis,axis=1)
+            if var=="by_cell_type_parameters":
+                ret.by_cell_type_zi=zis
+            elif var=="by_cre_parameters":
+                ret.by_cre_zi=zis
 
         
         #min & max nb and add to the return object
@@ -74,10 +99,17 @@ class Bounds:
 
         ret.theta=np.mean([ret.by_cell_type_theta,ret.by_cre_theta])
 
-        ## Parameters from training data ##    
+        ## Parameters from training data ##
+        # transfection, as proxied by number of MPRA barcodes detected per cell
         tfection_params=inp.training_data.describe_transfection()
         ret.transfection_nb_mu=tfection_params["nb_mu"]
         ret.transfection_nb_alpha=tfection_params["nb_alpha"]
+
+        #cells per cell type
+        ret.cells_per_cell_type=inp.training_data.data.groupby("cell_type")["cell_bc"].nunique()
+
+        #number of CREs
+        ret.num_cres=inp.training_data.data["cre_id"].nunique()
         
         return ret
 
