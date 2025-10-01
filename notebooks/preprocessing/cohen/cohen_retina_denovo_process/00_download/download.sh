4dd24e9d8ea8fcfeadf4a118f3d6f1c08fe62cb4
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-10-01 11:42:35 -0400

    +cohen download addtl. rep

--- notebooks/preprocessing/cohen/cohen_retina_denovo_process/00_download/download.sh
+++ notebooks/preprocessing/cohen/cohen_retina_denovo_process/00_download/download.sh
@@ -1,23 +1,18 @@
-#Downloading raw MPRA-seq fasta files for cohen retina data
-
-#GSM6614199 	retina single-cell transcriptome rep1 [scRNA-seq]
-#
-#GSM6614200 	retina single-cell transcriptome rep2 [scRNA-seq]
-
-#GSM6614201 	retina barcodes rep1 [scMPPRA]
-# SRR21787462
-#GSM6614202 	retina barcodes rep2 [scMPPRA]
-# ???
-
-#GSM6614203 	retina u6 barcodes rep1 [scMPPRA]
-# SRR21787461
-#GSM6614204 	retina u6 barcodes rep2 [scMPPRA]
-# SRR21787460
-
-
-#Let's start with the cohen retina data
-
-
-
+#!/bin/bash
+#SBATCH -t 24:00:00
+#SBATCH -p ycga
+#SBATCH -c 3
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-4
 
 DATA_ROOT="/home/mcn26/palmer_scratch/raw_recap/cohen_retina"
+
+module load SRA-Toolkit
+SRA_LIST="sra_runs.txt"
+SRA_RUN=$(sed -n ${SLURM_ARRAY_TASK_ID}p $SRA_LIST)
+echo "Downloading SRA run: $SRA_RUN"
+cd $DATA_ROOT
+prefetch $SRA_RUN
+echo "Extracting FASTQ files for SRA run: $SRA_RUN"
+fastq-dump --split-files --gzip $SRA_RUN
+echo "Completed processing SRA run: $SRA_RUN"
--- notebooks/preprocessing/cohen/cohen_retina_denovo_process/readme.md
+++ notebooks/preprocessing/cohen/cohen_retina_denovo_process/readme.md
@@ -1,2 +1,18 @@
-See obsidian://open?vault=science&file=Reverse-engineer%20cohen%20scmpra 
-lsrid bdHCwxVpTjgbQHr9w6qgLDJbZijv18ft
+DATA_ROOT="/home/mcn26/palmer_scratch/raw_recap/cohen_retina"
+
+#GSM6614199 	retina single-cell transcriptome rep1 [scRNA-seq]
+#
+#GSM6614200 	retina single-cell transcriptome rep2 [scRNA-seq]
+
+#GSM6614201 	retina barcodes rep1 [scMPPRA]
+# SRR21787462
+#GSM6614202 	retina barcodes rep2 [scMPPRA]
+# SRR32774353
+
+#GSM6614203 	retina u6 barcodes rep1 [scMPPRA]
+# SRR21787461
+#GSM6614204 	retina u6 barcodes rep2 [scMPPRA]
+# SRR21787460
+
+
+"Reverse-engineer cohen scmpra" in obsidian
