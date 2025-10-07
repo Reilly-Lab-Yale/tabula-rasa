1cfb2e6be69dc9920082adb128d9f06dcc4af5ee
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-10-07 13:41:30 -0400

    +faster split by size

---
+++ notebooks/preprocessing/cohen/cohen_retina_denovo_process/02_split_by_size/wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH --array=0-8
+#SBATCH --mem-per-cpu=1G
+#SBATCH -t 6:00:00
+#SBATCH -p ycga
+
+module load miniconda
+conda activate speedracer
+
+inroot="/home/mcn26/palmer_scratch/raw_recap/cohen_retina/raw_fastq"
+files=($(ls $inroot))
+file="${files[$SLURM_ARRAY_TASK_ID]}"
+outroot="/home/mcn26/palmer_scratch/raw_recap/cohen_retina/length_split_fastq"
+
+cat $inroot/$file | pypy3 split_by_size.py $file .
