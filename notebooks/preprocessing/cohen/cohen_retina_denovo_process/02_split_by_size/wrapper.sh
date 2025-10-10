52887baaee7a19baaf549de3d8213838bfd75ff7
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-10-10 15:57:29 -0400

    reruns filter, break by length steps

--- notebooks/preprocessing/cohen/cohen_retina_denovo_process/02_split_by_size/wrapper.sh
+++ notebooks/preprocessing/cohen/cohen_retina_denovo_process/02_split_by_size/wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -c 1
-#SBATCH --array=0-8
+#SBATCH --array=0-7
 #SBATCH --mem-per-cpu=1G
 #SBATCH -t 6:00:00
 #SBATCH -p ycga
@@ -8,7 +8,7 @@
 module load miniconda
 conda activate speedracer
 
-inroot="/home/mcn26/palmer_scratch/raw_recap/cohen_retina/raw_fastq"
+inroot="/home/mcn26/palmer_scratch/raw_recap/cohen_retina/filtered_fastq"
 files=($(ls $inroot))
 file="${files[$SLURM_ARRAY_TASK_ID]}"
 outroot="/home/mcn26/palmer_scratch/raw_recap/cohen_retina/length_split_fastq"
