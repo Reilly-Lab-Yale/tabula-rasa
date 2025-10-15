e5815e37af8174da244080be775cfab8670b175e
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-10-15 10:32:41 -0400

    +extract step

--- notebooks/preprocessing/cohen/cohen_retina_denovo_process/08_barcode_count/extract.sh
+++ notebooks/preprocessing/cohen/cohen_retina_denovo_process/08_barcode_count/extract.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
-#SBATCH -t 12:00:00
-#SBATCH --array=0-2
+#SBATCH -t 4:00:00
+#SBATCH --array=0-3
 #SBATCH -c 10
 #SBATCH -p ycga
+#SBATCH -J counts
 #SBATCH --mem-per-cpu=1G
 
 #After the last step, read-names now have extracted barcodes
-#here I extract & count unique barcode combos
+#here I extract & count unique barcode combos. 
+#Note that umitools puts the bacrodes in the field-1 (lines that start with @) in both R1 & 2 output
+#so we only need to process one read of the pair for each. I have arbitrarially chosen R1
 
-filenames=("SRR21787460_processed_R1.fastq" "SRR21787461_processed_R1.fastq" "SRR21787462.fastq")
+paths=("main_bc_extract/SRR21787462/SRR21787462.processed_R1.fastq" "main_bc_extract/SRR32774353/SRR32774353.processed_R1.fastq" "u6_bc_extract/SRR21787461/SRR21787461.processed_R1.fastq" "u6_bc_extract/SRR21787462/SRR21787462.processed_R1.fastq" )
 
-inp_root="/home/mcn26/palmer_scratch/tabula_data/raw_recap/cohen_retina/main_bc_extract"
-out_root="/home/mcn26/palmer_scratch/tabula_data/raw_recap/cohen_retina/counts"
+inp_root="/home/mcn26/palmer_scratch/raw_recap/cohen_retina/"
+out_root="/home/mcn26/palmer_scratch/raw_recap/cohen_retina/counts"
 
-idx=0
-filename=${filenames[${SLURM_ARRAY_TASK_ID}]}
+
+filename=${paths[${SLURM_ARRAY_TASK_ID}]}
 
 echo "Processing ${filename}"
+
+#at this step we process both u6 and non-u6 since umitools smushes all umis together, making
+#some processing identical for both
+
+#examples:
+#normal mpra looks like
+#@SRR21787462.71_TTGCATTAGTGTACAA_AGCCGTGATATAATTGGTAGCGAAAATTGAAACTAGACCGGATT NB501065:669:HJL2TBGXL:1:11101:11858:1071 length=28
+#then splitting `cut -d'_' -f2-3`
+#TTGCATTAGTGTACAA, AGCCGTGATATAATTGGTAGCGAAAATTGAAACTAGACCGGATT NB501065:669:HJL2TBGXL:1:11101:11858:1071 length=28
+#then splitting `| cut -d' '  -f1 `
+#TTGCATTAGTGTACAA, AGCCGTGATATAATTGGTAGCGAAAATTGAAACTAGACCGGATT
+#where TTGCATTAGTGTACAA is 16bp <cell_1>, cbc, AGCCGTGATATA is 12bp, <umi_1> umi, ATTGGTAG is 8bp <umi_2> pBC, CGAAAATTGAAACTAGACCGGATT is 24 bp, <umi_3> rBC
+
+#U6 looks like
+#@SRR21787461.241_ATCCGTCGTGGTGAGT_TTGTCAAAGACTACGTATGG NB501801:559:H3VVTBGXM:1:11101:10557:1087 length=28
+#processing similarly, we get
+#ATCCGTCGTGGTGAGT, TTGTCAAAGACTACGTATGG
+#where ATCCGTCGTGGTGAGT is 16bp <cell_1>, TTGTCAAAGACT is 12bp actual umi, ACGTATGG is 8bp pbc
+
+base=$(basename ${filename})
 
 cat ${inp_root}/${filename} \
 	| grep -E "^@" \
@@ -24,5 +47,5 @@ cat ${inp_root}/${filename} \
 	| cut -d' '  -f1 \
 	| sort \
 	| uniq -c \
-	> ${out_root}/${filename}.count
+	> ${out_root}/${base}.count
 
