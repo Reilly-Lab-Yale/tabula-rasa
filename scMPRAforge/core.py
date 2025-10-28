d9a09c922f451c1c85417f8028ee949ac5976c30
erin-n-gilbertson <erin.gilbertson@yale.edu>
2025-10-28 13:46:07 -0400

    fix tab/space issue

--- scMPRAforge/core.py
+++ scMPRAforge/core.py
@@ -1685,7 +1685,7 @@ class ortho:
             raise RuntimeError("precompute_wald requires by_cell_type and by_cre models to be present.")
 
         by_ct = {}
-		by_cr = {}
+        by_cr = {}
         #Very Lame retry hack due to extremely rare failures in _hessian_se
         #TODO: debug intermitant `AlreadyExistsError` properly once precompute_wald is faster.
         
