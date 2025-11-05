7955e67e14fc64e28f3b4464bf75c9af4bba3962
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-11-05 17:53:20 -0500

    +docs

--- notebooks/preprocessing/cohen/cohen_retina_denovo_process/01b_preseq/1_make_hist.sh
+++ notebooks/preprocessing/cohen/cohen_retina_denovo_process/01b_preseq/1_make_hist.sh
@@ -4,6 +4,14 @@
 #SBATCH --array=0-7
 #SBATCH -t 4:00:00
 
+#previous step counts occurances of reads, producing
+#5 AATGATGA
+#3 TAGATAGATATGAG
+#etc...
+#we call those ocurances read counts
+#this script makes a "histogram": it counts 
+#how many times we see a read count of n
+
 root=/home/mcn26/palmer_scratch/raw_recap/cohen_retina
 inp=${root}/uniq_counts
 oup=${root}/histograms
@@ -12,5 +20,6 @@ current_file=${files[$SLURM_ARRAY_TASK_ID]}
 name=$(basename "$current_file" ".txt")
 
 echo "Processing ${current_file}"
-cat ${inp}/${current_file} | cut -f1 | sort | uniq -c | awk '{print $2 "\t" $1}' > ${oup}/${name}.hist
-# we reverse the order because preseq expects 
+cat ${inp}/${current_file} | cut -f1 | sort | uniq -c | awk '{print $2 "\t" $1}' | sort -g > ${oup}/${name}.hist
+# we reverse the order (print 2 then 1) because preseq expects "read count" then "frequency"
+# we employ one final sort because preseq is a diva and wants the data pre-sorted by read count
