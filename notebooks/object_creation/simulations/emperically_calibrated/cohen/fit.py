900dc37c661c58d94990fe6a9f475fa3baad02fd
Mackenzie Noon <mackenziecnoon@gmail.com>
2026-02-09 15:08:18 -0500

    no tokens

--- notebooks/object_creation/simulations/emperically_calibrated/cohen/fit.py
+++ notebooks/object_creation/simulations/emperically_calibrated/cohen/fit.py
@@ -6,23 +6,22 @@ from pathlib import Path
 import time
 
 def main():
-    with dask.config.set({"distributed.worker.resources.FIT":1}):
-        cluster=LocalCluster()
-        client=Client(cluster)
-        data_root=Path("/nfs/roberts/project/pi_skr2/shared/tabula_data")
-        print(client.dashboard_link,flush=True)
-        sim=scm.de_novo_simulation(location=data_root,
-                                   name="twothird_pow_sim_2026-02-05",
-                                   client=client)
-        print("LOADED",flush=True)
-        sim.fit_orthos(serial_orthos=False,direction="by_cell_type")
-        print("SUBMITTED",flush=True)
-        sim.save()
-        print("DONE",flush=True)
-        time.sleep(60)
-        client.close()
-        cluster.close()
-        time.sleep(60)
+    cluster=LocalCluster()
+    client=Client(cluster)
+    data_root=Path("/nfs/roberts/project/pi_skr2/shared/tabula_data")
+    print(client.dashboard_link,flush=True)
+    sim=scm.de_novo_simulation(location=data_root,
+                               name="twothird_pow_sim_2026-02-05",
+                               client=client)
+    print("LOADED",flush=True)
+    sim.fit_orthos(serial_orthos=False,direction="by_cell_type")
+    print("SUBMITTED",flush=True)
+    sim.save()
+    print("DONE",flush=True)
+    time.sleep(60)
+    client.close()
+    cluster.close()
+    time.sleep(60)
 
 if __name__ == "__main__":
     main()
