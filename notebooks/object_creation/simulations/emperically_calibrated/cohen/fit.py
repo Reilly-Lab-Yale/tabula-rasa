8ed4eb14e2b82c4188d5e1db7b4ba98a1d0bfc96
Mackenzie Noon <mackenziecnoon@gmail.com>
2026-02-09 15:14:42 -0500

    +performance report

--- notebooks/object_creation/simulations/emperically_calibrated/cohen/fit.py
+++ notebooks/object_creation/simulations/emperically_calibrated/cohen/fit.py
@@ -1,27 +1,33 @@
 import scMPRAforge as scm
 import dask
 from dask_jobqueue import SLURMCluster
-from dask.distributed import Client, LocalCluster
+from dask.distributed import Client, LocalCluster, performance_report
 from pathlib import Path
 import time
+import os
 
 def main():
     cluster=LocalCluster()
     client=Client(cluster)
-    data_root=Path("/nfs/roberts/project/pi_skr2/shared/tabula_data")
-    print(client.dashboard_link,flush=True)
-    sim=scm.de_novo_simulation(location=data_root,
-                               name="twothird_pow_sim_2026-02-05",
-                               client=client)
-    print("LOADED",flush=True)
-    sim.fit_orthos(serial_orthos=False,direction="by_cell_type")
-    print("SUBMITTED",flush=True)
-    sim.save()
-    print("DONE",flush=True)
-    time.sleep(60)
-    client.close()
-    cluster.close()
-    time.sleep(60)
+    
+    job_id = os.environ.get('SLURM_JOB_ID', 'local')
+    report_filename = f"dask-report-job-{job_id}.html"
+    
+    with performance_report(filename=report_filename):
+        data_root=Path("/nfs/roberts/project/pi_skr2/shared/tabula_data")
+        print(client.dashboard_link,flush=True)
+        sim=scm.de_novo_simulation(location=data_root,
+                                   name="twothird_pow_sim_2026-02-05",
+                                   client=client)
+        print("LOADED",flush=True)
+        sim.fit_orthos(serial_orthos=False,direction="by_cell_type")
+        print("SUBMITTED",flush=True)
+        sim.save()
+        print("DONE",flush=True)
+        time.sleep(60)
+        client.close()
+        cluster.close()
+        time.sleep(60)
 
 if __name__ == "__main__":
     main()
