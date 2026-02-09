3b0ad45d02b980eccf16713c8a133345a9e8a55d
Mackenzie Noon <mackenziecnoon@gmail.com>
2026-02-09 15:32:24 -0500

    +resources

--- notebooks/object_creation/simulations/emperically_calibrated/cohen/fit.py
+++ notebooks/object_creation/simulations/emperically_calibrated/cohen/fit.py
@@ -7,27 +7,28 @@ import time
 import os
 
 def main():
-    cluster=LocalCluster()
-    client=Client(cluster)
-    
-    job_id = os.environ.get('SLURM_JOB_ID', 'local')
-    report_filename = f"dask-report-job-{job_id}.html"
-    
-    with performance_report(filename=report_filename):
-        data_root=Path("/nfs/roberts/project/pi_skr2/shared/tabula_data")
-        print(client.dashboard_link,flush=True)
-        sim=scm.de_novo_simulation(location=data_root,
-                                   name="twothird_pow_sim_2026-02-05",
-                                   client=client)
-        print("LOADED",flush=True)
-        sim.fit_orthos(serial_orthos=False,direction="by_cell_type")
-        print("SUBMITTED",flush=True)
-        sim.save()
-        print("DONE",flush=True)
-        time.sleep(60)
-        client.close()
-        cluster.close()
-        time.sleep(60)
+    with dask.config.set({"distributed.worker.resources.FIT":1,"CELL_DESIGN":1}):
+        cluster=LocalCluster()
+        client=Client(cluster)
+        
+        job_id = os.environ.get('SLURM_JOB_ID', 'local')
+        report_filename = f"dask-report-job-{job_id}.html"
+        
+        with performance_report(filename=report_filename):
+            data_root=Path("/nfs/roberts/project/pi_skr2/shared/tabula_data")
+            print(client.dashboard_link,flush=True)
+            sim=scm.de_novo_simulation(location=data_root,
+                                       name="twothird_pow_sim_2026-02-05",
+                                       client=client)
+            print("LOADED",flush=True)
+            sim.fit_orthos(serial_orthos=False,direction="by_cell_type")
+            print("SUBMITTED",flush=True)
+            sim.save()
+            print("DONE",flush=True)
+            time.sleep(60)
+            client.close()
+            cluster.close()
+            time.sleep(60)
 
 if __name__ == "__main__":
     main()
