804ce982287fd73204090056521b186162adfb7e
Mackenzie Noon <mackenziecnoon@gmail.com>
2026-02-11 18:20:01 -0500

    memory consilidation

--- notebooks/object_creation/simulations/emperically_calibrated/cohen/fit.py
+++ notebooks/object_creation/simulations/emperically_calibrated/cohen/fit.py
@@ -15,8 +15,8 @@ def main():
     print("psutil affinity     =", len(psutil.Process().cpu_affinity()),flush=True)
 
     cluster=LocalCluster(
-        n_workers=6,
-        threads_per_worker=10,
+        n_workers=4,
+        threads_per_worker=5,
         resources={"CELL_DESIGN": 1},
         processes=True
     )
