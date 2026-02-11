52f4e1ed0df913f3e4ce96171c9ec6fa45304203
Mackenzie Noon <mackenziecnoon@gmail.com>
2026-02-10 22:22:48 -0500

    +big df resource

--- notebooks/object_creation/simulations/emperically_calibrated/cohen/fit.py
+++ notebooks/object_creation/simulations/emperically_calibrated/cohen/fit.py
@@ -17,8 +17,11 @@ def main():
     cluster=LocalCluster(
         n_workers=6,
         threads_per_worker=10,
+        resources={"CELL_DESIGN": 1},
         processes=True
     )
+    
+
     client=Client(cluster)
     job_id = os.environ.get('SLURM_JOB_ID', 'local')
     report_filename = f"dask-report-job-{job_id}.html"
@@ -27,8 +30,8 @@ def main():
         data_root=Path("/nfs/roberts/project/pi_skr2/shared/tabula_data")
         print(client.dashboard_link,flush=True)
         sim=scm.de_novo_simulation(location=data_root,
-                                   name="twothird_pow_sim_2026-02-05",
-                                   client=client)
+                                name="twothird_pow_sim_2026-02-05",
+                                client=client)
         print("LOADED",flush=True)
         sim.fit_orthos(serial_orthos=False,direction="by_cell_type")
         print("SUBMITTED",flush=True)
