d77af55ef354558bed6b1d72f738d8ff358b8a93
Mackenzie Noon <mackenziecnoon@gmail.com>
2026-04-10 17:02:58 -0400

    +re-run pairwise power w/ new bounds

--- analyses/simulation/variant_power/shendure/wrap_shendure_pairwise_power.sh
+++ analyses/simulation/variant_power/shendure/wrap_shendure_pairwise_power.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=shend_pw
-#SBATCH --partition=ycga
-#SBATCH --time=12:00:00
-#SBATCH --cpus-per-task=2
+#SBATCH --partition=priority
+#SBATCH --account=prio_skr2
+#SBATCH --time=14:00:00
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=32G
-#SBATCH --output=slurm-pw-%j.out
-#SBATCH --error=slurm-pw-%j.err
+#SBATCH --exclude=a1132u18n02
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
 
 module load miniconda
 conda activate tz
 
-jupyter nbconvert --to notebook --execute --inplace \
-    --ExecutePreprocessor.timeout=-1 \
-    shendure_pairwise_power_mwu.ipynb
-
+python shendure_pairwise_power_mwu.py all
 exit_code=$?
 
 if [ $exit_code -eq 0 ]; then
@@ -21,7 +20,7 @@ if [ $exit_code -eq 0 ]; then
         notify-job "shendure_pairwise_power_mwu completed successfully on $(hostname)"
 else
     NTFY_TITLE="Shendure pairwise power FAILED" NTFY_TAGS="warning,x" \
-        notify-job "shendure_pairwise_power_mwu exited with code $exit_code on $(hostname). Check slurm-pw-${SLURM_JOB_ID}.err"
+        notify-job "shendure_pairwise_power_mwu exited with code $exit_code on $(hostname). Check slurm-${SLURM_JOB_ID}.err"
 fi
 
 echo "EXITING SHELL"
